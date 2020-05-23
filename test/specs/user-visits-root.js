473bdd05b739479bf7414a1ef2b64242d1421c90
Noel <loera.noel@gmail.com>
2020-05-22 19:34:23 -0700

    05-22-2020: The feature testing level for the application has been completed and now needs to have the server level use CRUD to make sure features work.

--- test/specs/user-visits-root.js
+++ test/specs/user-visits-root.js
@@ -1,5 +1,15 @@
 const {assert} = require('chai');
 
+/*
+describe('webdriver.io page', () => {
+    it('should have the right title', () => {
+        browser.url('https://webdriver.io')
+        const title = browser.getTitle()
+        expect(browser).toHaveTitle('WebdriverIO · Next-gen browser and mobile automation test framework for Node.js');
+    })
+})
+*/
+
 //If this does not work try checking the WebDriver IO CLI
 
 //This is the feature testing when a user will visit the root
@@ -13,21 +23,27 @@ describe("User Enters Root",()=>{
             const quote = "All types of knowledge, ultimately mean self knowledge.";
             const author = "Bruce Lee";
             const source = "The Lost Interview (1971)";
+            //Saves the element queries into accesible variables
+            const quoteElem = $('#quote');
+            const authorElem = $('#author');
+            const sourceElem = $('#source');
+            const buttonElem = $('#submit');
+            const allQuotesElem = $('#quotes')
             //EXCERCISE
             //Directs browser to travel to the root file
-            browser.url('/');
+            browser.url('file:///Users/noel/Documents/WebDesignProjects/quote_keeper/index.html');
             //Sets the value of the textarea and input elements with corresponding values
-            browser.setValue('textarea[id=quote]', quote);
-            browser.setValue('input[id=author]', author);
-            browser.setValue('source[id=source]', source);
+            quoteElem.addValue(quote);
+            authorElem.addValue(author);
+            sourceElem.addValue(source);
             //Mimics a user click on a submit input element
-            browser.click('input[type=submit]');
+            buttonElem.click();
             //VERIFY
             //This will esure that the values match the desired values with assertions where all "quotes" should appear
-            const quotes = browser.getText('#quotes');
+            const quotes = allQuotesElem.getText();
             assert.include(quotes, quote);
             assert.include(quotes, author);
-            assert.include(quotes.source);
+            assert.include(quotes, source);
         })
     })
 })
