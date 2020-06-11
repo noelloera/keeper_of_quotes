fda8935db91a0fafa7b177b35f75f1dd09847121
noelloera <loera.noel@gmail.com>
2020-06-11 15:28:41 -0600

    06-11-2020: Added the Javascript layer for logic in the HTTP calls to the server

--- server/index.js
+++ server/index.js
@@ -11,16 +11,13 @@ const PORT = process.env.PORT || 4001;
 //Middleware for setting static directories
 app.use(bodyParser.json());
 
-//app.use(express.static(path.join(__dirname,'public')))
+app.use(express.static(path.join(__dirname,'public')))
 
 app.get('/',(req, res)=>{
     res.sendFile(path.join(__dirname,'../public/index.html'))
 })
 
 app.post('/', (req, res)=>{
-    //check if there is values within the body, then push to array
-    console.log(req.body.author)
-    //allQuotes.push(req.body);
     console.log("...posting something")
 })
 
