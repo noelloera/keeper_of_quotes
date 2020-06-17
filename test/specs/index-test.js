bfea968ed667cec22e7c5948410508ec281a2f5a
noelloera <loera.noel@gmail.com>
2020-06-16 20:59:12 -0600

    06-16-2020: Added the server POST test to the testing suite for TDD server layer.

--- test/specs/index-test.js
+++ test/specs/index-test.js
@@ -11,7 +11,15 @@ describe('/', ()=>{
             const quote = "If one loves, one need not to have an ideology of love";
             const author =  "Bruce Lee";
             const source = "The Warrior Within";
-            fetch('http://localhost:4001/', {method: POST, body:{quote,author,source}})
+            //Attempt to make a POST request to the root of the page
+            //Separate the body of information being sent
+            const data = {quote, author, source};
+            //When making a fetch request it's wise to separate the options
+            let options ={
+                method: 'POST',
+                body: JSON.stringify(data);
+            }
+            let response = await fetch('http://localhost:4001/', {method: POST, body:{quote,author,source}})
                 .then(res => res.json())
                 .then(console.log())
             
