ce713bcc6b770ca72ac93f6e800220c4a0f4aa8e
Noel <loera.noel@gmail.com>
2020-07-09 20:25:56 -0700

    07-09-2020: The feature test was refactored

--- test/specs/feature-test.js
+++ test/specs/feature-test.js
@@ -1,26 +1,13 @@
 const { assert } = require("chai");
 const fs = require("fs");
 
-//If this does not work try checking the WebDriver IO CLI
-
 //This is the feature testing when a user will visit the root
 describe("The applications features exist and work correctly", () => {
   const filepath =
-  "file:///Users/noel/Documents/WebDesignProjects/quote_keeper/public/index.html";
-  beforeEach(()=>{
+    "file:///Users/noel/Documents/WebDesignProjects/quote_keeper/public/index.html";
+  beforeEach(() => {
     browser.url(filepath);
-  })
-  
-  const quote = "All types of knowledge, ultimately mean self knowledge.";
-  const author = "Bruce Lee";
-  const source = "The Lost Interview (1971)";
-  //Saves the element queries into accesible variables
-  const quoteElem = $("#quote");
-  const authorElem = $("#author");
-  const sourceElem = $("#source");
-  const buttonElem = $("#submit");
-  const allQuotesElem = $("#quotes");
-  
+  });
   //Verifies that the application loads when being accessing html file
   describe("The application loads the file for live view", () => {
     it("Should load when seeing live view", () => {
@@ -29,28 +16,19 @@ describe("The applications features exist and work correctly", () => {
         if (fs.existsSync(filepath)) {
           assert.isOk(true, filepath);
         }
-      } catch {
-        error;
+      } catch (error) {
         console.log(error);
       }
     });
-  });
-  //Verifies that the application contains the elements it should
-  describe("The application loads the elements", () => {
-    beforeEach(()=>{
-      browser.url(filepath);
-    })
-    const quote = "All types of knowledge, ultimately mean self knowledge.";
-    const author = "Bruce Lee";
-    const source = "The Lost Interview (1971)";
-    //Saves the element queries into accesible variables
-    const quoteElem = $("#quote");
-    const authorElem = $("#author");
-    const sourceElem = $("#source");
-    const buttonElem = $("#submit");
-    const allQuotesElem = $("#quotes");
+    //Use the is displayed property
     it("Should contain all the elements references", () => {
-      if ((quoteElem, authorElem, sourceElem, buttonElem, allQuotesElem)) {
+      const quoteElem = Document.querySelector("#quote").length;
+      const authorElem = Document.querySelector("#author").length;
+      const sourceElem = Document.querySelector("#source").length;
+      const buttonElem = Document.querySelector("#submit").length;
+      const allQuotesElem = Document.querySelector("#quotes").length;
+      console.log(quoteElem, authorElem, sourceElem);
+      if ((quoteElem, authorElem, sourceElem)) {
         assert.isTrue(true, quoteElem);
         assert.isTrue(true, authorElem);
         assert.isTrue(true, sourceElem);
@@ -61,18 +39,27 @@ describe("The applications features exist and work correctly", () => {
       }
     });
     it("Should add values to the input fields", () => {
-      console.log(quoteElem)
-
+      //Will include test code for adding values
     });
-    it("Should click the submit button and expect for quote to be posted", () => {});
+    it("Should click the submit button and expect for quote to be posted", () => {
+      //Will contain the test code for the click
+    });
   });
 
   //This will test functionality when a Quote is submitted
   describe("User Inputs information ", () => {
     it("Should POST the Quote and meta to the database", () => {
+      const quote = "All types of knowledge, ultimately mean self knowledge.";
+      const author = "Bruce Lee";
+      const source = "The Lost Interview (1971)";
+      //Saves the element queries into accesible variables
+      const quoteElem = $("#quote");
+      const authorElem = $("#author");
+      const sourceElem = $("#source");
+      const buttonElem = $("#submit");
+      const allQuotesElem = $("#quotes");
       //EXCERCISE
       //Directs browser to travel to the root file
-      browser.url("http://localhost:5500/");
       //Sets the value of the textarea and input elements with corresponding values
       quoteElem.addValue(quote);
       authorElem.addValue(author);
